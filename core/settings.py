d4feb8ba0a27f2351373a8c630666b8536c90321
hamedsalim1999 <hamediran1999@gmail.com>
2021-04-29 07:47:38 -0700

    add env file and mysql database on setting file

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('Mysql_Name', 'local_name'),
+        'USER': os.environ.get('Mysql_User', 'local_user'),
+        'PASSWORD': os.environ.get('Mysql_Password', 'local_pass'),
+        'HOST': os.environ.get('Mysql_Host', 'local_host'),
+        'PORT': '3306'
     }
 }
 
