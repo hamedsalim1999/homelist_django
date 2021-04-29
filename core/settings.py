612e7d5f459775928e73925fb98d21954b70790b
hamedsalim <hamediran1999@gmail.com>
2021-04-29 19:37:17 +0430

    cahge to postgresql

--- core/settings.py
+++ core/settings.py
@@ -78,14 +78,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('Mysql_Name', 'local_name'),
-        'USER': os.environ.get('Mysql_User', 'local_user'),
-        'PASSWORD': os.environ.get('Mysql_Password', 'local_pass'),
-        'HOST': os.environ.get('Mysql_Host', 'local_host'),
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
-}
+    }
+
 
 
 # Password validation
