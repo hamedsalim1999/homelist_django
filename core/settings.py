30c2be3533c3e5ca07c11a4616d60e741f34d27f
hamedsalim <hamediran1999@gmail.com>
2021-04-30 10:46:36 +0430

    change static file root

--- core/settings.py
+++ core/settings.py
@@ -128,13 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'media/collectstatic')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media/static/'),
-)
-MEDIA_ROOT = os.path.join(BASE_DIR , 'media/upload/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Default primary key field type
