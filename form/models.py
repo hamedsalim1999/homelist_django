03a9119e0aaa7f4d6a0f5b6b9c42fa8fde1a577c
hamedsalim <hamediran1999@gmail.com>
2021-05-01 20:32:55 +0430

    choises filed

--- form/models.py
+++ form/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-from phone_field import PhoneField
 # Create your models here.
 from jalali_date import datetime2jalali, date2jalali
+activate=(
+("s","سازنده"),
+("ma","مصالح فروش"),
+("am","املاکی"),
+("sa","سایر"),
+)
+
+
 
 class Form(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,11 +16,15 @@ class Form(models.Model):
     region = models.PositiveBigIntegerField(verbose_name = "منطقه شهرداری")
     district = models.PositiveBigIntegerField(verbose_name = "ناحیه شهرداری ")
     part = models.PositiveBigIntegerField(verbose_name = "بخش شهر داری ")
-    type_of_activate  = models.TextField(verbose_name = "نوع فعالیت")
+    type_of_activate  = models.CharField(
+        max_length = 2,
+        choices = activate,
+        verbose_name = "نوع فعالیت",
+        )
     responsible_name = models.CharField(max_length=1024,verbose_name = "نام مسئول")
-    responsible_phone = PhoneField(blank=True, help_text='Contact phone number',verbose_name = "شماره تماس مسئول")
-    manager_name = models.CharField(max_length=1024, verbose_name = "نام مدیر")
-    manager_phone = PhoneField(blank=True, help_text='Contact phone number',verbose_name = "شماره تماس مدیر")
+    responsible_phone =  models.CharField(blank=True ,verbose_name="شماره تماس مسئول",max_length = 12)
+    manager_name = models.CharField(max_length=1024,verbose_name = "نام مدیر")
+    manager_phone =  models.CharField(blank=True,verbose_name = "شماره تماس مدیر",max_length = 12)
     addres = models.TextField(verbose_name = "نشانی")
 
     def get_created_jalali(self):
@@ -26,4 +37,15 @@ class Form(models.Model):
         ordering = ('-create',)
         verbose_name = 'فرم'
         verbose_name_plural = 'فرم ها'
+
+def get_defualt_objects(obj):
+    try:
+        obj.objects.last()
+    except obj.DoesNotExist:
+        None
+
+
+def __save__(self):
+    defualt_obj = Form.objects.order_by('id')[0]
+    Form.region = defualt_obj.region
 
